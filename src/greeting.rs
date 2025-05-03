419a6f30fe73d934c69780eec401031abf94db1e
Arne Nilsen <arne.nilsen@skatteetaten.no>
2025-05-03 09:22:00 +0200

    Fixing dataaccess

--- src/greeting.rs
+++ src/greeting.rs
@@ -1,38 +1,109 @@
-use std::time::Duration;
-use actix_web::{get, HttpResponse, ResponseError};
+use crate::greeting::ApiError::ApplicationError;
 use actix_web::http::header::ContentType;
 use actix_web::http::StatusCode;
 use actix_web::web::Data;
-use derive_more::{Display};
+use actix_web::{get, web, HttpResponse, ResponseError};
+use chrono::{DateTime, Utc};
+use derive_more::Display;
 use log::{error, info};
-use sqlx::{Executor, Pool};
-use crate::greeting::ApiError::ApplicationError;
+use once_cell::sync::Lazy;
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use sqlx::query::Query;
+use sqlx::{query, Execute, Executor, PgPool, Pool, Postgres, QueryBuilder, Row};
+use std::time::Duration;
+use tracing_subscriber::fmt::format;
+use utoipa::ToSchema;
+use validator_derive::Validate;
 
+#[derive(Validate, Serialize, Deserialize, Clone, ToSchema, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct LoggQuery {
+    #[validate(range(min = 1))]
+    offset: i64,
+    #[validate(range(min = 1, max = 1000))]
+    limit: i64,
+    #[validate(regex(path = *DIRECTION, message = "Invalid direction"))]
+    direction: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, ToSchema, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct LoggEntry {
+    id: i64,
+    greeting_id: i64,
+    #[schema(value_type = String, format = DateTime)]
+    created: DateTime<Utc>,
+}
+
+static DIRECTION: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(asc|desc)$").unwrap());
 #[utoipa::path(
     get,
     path = "/logs",
+    params(
+        ("logg_query" = LoggQuery, Query,)
+    ),
     responses(
-        (status = 200, description = "Greetings", body = String),
+        (status = 200, description = "Greetings", body = LoggEntry),
         (status = NOT_FOUND, description = "Greetings was not found")
     )
 )]
 #[get("/logs")]
-pub async fn list_log_entries(data: Data<Pool<sqlx::Postgres>>) -> Result<HttpResponse, ApiError> {
-    let r = data.execute("\
-        SELECT id, greeting_id
-        FROM LOGG
-        ORDER BY id
-    ").await?;
+pub async fn list_log_entries(
+    data: Data<Box<Pool<Postgres>>>,
+    logg_query: web::Query<LoggQuery>,
+) -> Result<HttpResponse, ApiError> {
+    let direction = SqlDirection::value_of(&logg_query.direction);
 
-    Ok(HttpResponse::Ok().json(""))
+    let mut logg_sql: QueryBuilder<Postgres> =
+        QueryBuilder::new("SELECT id, greeting_id, opprettet FROM LOGG");
+
+    logg_sql.push(format!(" WHERE id {} ", direction.operator));
+    logg_sql.push_bind(logg_query.offset );
+    logg_sql.push(format!(" ORDER BY id {}", direction.order));
+    logg_sql.push(" LIMIT ");
+    logg_sql.push_bind(logg_query.limit);
+
+    let r = data
+        .fetch_all(logg_sql.build())
+        .await
+        .map(|res| res.iter().map(|v| 
+            LoggEntry { id: v.get(0) , greeting_id: v.get(1), created: v.get(2) }).collect::<Vec<_>>()
+        )
+        .map_err(|e| -> ApiError {
+            error!("{}", e);
+            ApplicationError(e)
+        })?;
+    
+
+    Ok(HttpResponse::Ok().json(r))
 }
 
+struct SqlDirection {
+    order: String,
+    operator: String,
+}
+
+impl SqlDirection {
+    fn value_of(direction: &str) -> SqlDirection {
+        match direction {
+            "forward" => SqlDirection {
+                order: String::from("ASC"),
+                operator: String::from(">="),
+            },
+            "backward" => SqlDirection {
+                order: String::from("DESC"),
+                operator: String::from("<="),
+            },
+            _ => panic!("Invalid direction"),
+        }
+    }
+}
 
 #[derive(Debug, Display)]
 pub enum ApiError {
-    ApplicationError(sqlx::Error)
+    ApplicationError(sqlx::Error),
 }
-
 
 impl ResponseError for ApiError {
     fn status_code(&self) -> StatusCode {
@@ -55,21 +126,24 @@ impl From<sqlx::Error> for ApiError {
     }
 }
 
-pub async fn generate_logg(pool: Box<Pool<sqlx::Postgres>>) {
+pub async fn generate_logg(pool: Data<Box<Pool<sqlx::Postgres>>>) {
     loop {
         tokio::time::sleep(Duration::from_secs(5)).await;
 
         match pool.begin().await {
-            Err(e) => error!("{}" ,e),
+            Err(e) => error!("{}", e),
             Ok(mut transaction) => {
-                sqlx::query("do
+                sqlx::query(
+                    "do
                         $$
                             begin
                                 perform public.generate_logg();
                             end
-                        $$;")
-
-                    .execute(&mut *transaction).await.expect("Failed executing statement");
+                        $$;",
+                )
+                    .execute(&mut *transaction)
+                    .await
+                    .expect("Failed executing statement");
                 info!("Generating log");
                 transaction.commit().await.expect("");
             }
--- src/main.rs
+++ src/main.rs
@@ -14,23 +14,23 @@ async fn main() -> std::io::Result<()> {
     #[derive(OpenApi)]
     #[openapi(
         info(description = "Greeting Api description"),
-        paths(greeting::list_log_entries, greeting::list_log_entries),
-        // components(schemas(api::GreetingDto))
+        paths(greeting::list_log_entries),
+        components(schemas(greeting::LoggQuery, greeting::LoggEntry))
     )]
 
     struct ApiDoc;
 
     let app_config = Settings::new();
 
-    // open_telemetry::init_otel(&app_config).await;
-    greeting_otel::init_otel(&app_config.otel_collector.oltp_endpoint,"greeting_api", &app_config.kube.my_pod_name).await;
-    let pool = Box::new(db::init_db(app_config.db.database_url.clone()).await.expect("Expected db pool"));
+
+    // greeting_otel::init_otel(&app_config.otel_collector.oltp_endpoint,"greeting_api", &app_config.kube.my_pod_name).await;
+    let pool = web::Data::new(Box::new(db::init_db(app_config.db.database_url.clone()).await.expect("Expected db pool")));
 
     let log_generator_handle = greeting::generate_logg(pool.clone());
 
     let server_handle = HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(pool.clone()))
+            .app_data(pool.clone())
             .service(greeting::list_log_entries)
             .service(
                 SwaggerUi::new("/swagger-ui/{_:.*}")
@@ -38,12 +38,12 @@ async fn main() -> std::io::Result<()> {
             )
 
     })
-        .bind(("127.0.0.1", 8080))?
+        .bind(("127.0.0.1", 8081))?
         .run();
 
     let r = join!(log_generator_handle, server_handle);
 
-    global::shutdown_tracer_provider();
+    // global::shutdown_tracer_provider();
     // logger_provider.shutdown();
     Ok(())
 }
